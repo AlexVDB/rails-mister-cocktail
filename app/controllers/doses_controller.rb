89284027adbf228f653f7558c415e2f68e95bc74
Alexandra Vandenbilcke <alexandra.vandenbilcke@gmail.com>
2019-08-15 15:42:06 +0200

    Add controllers Cocktails & Doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :destroy]
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @dose.delete
+
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:show, :new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+
+
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # get 'cocktails/edit'
+  # get 'cocktails/update'
+  # get 'cocktails/delete'
+
+  # get 'ingredients/index'
+  # get 'ingredients/show'
+  # get 'ingredients/new'
+  # get 'ingredients/create'
+  # get 'ingredients/edit'
+  # get 'ingredients/update'
+  # get 'ingredients/delete'
+
+  # get 'doses/index'
+  # get 'doses/show'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/edit'
+  # get 'doses/update'
+  # get 'doses/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
